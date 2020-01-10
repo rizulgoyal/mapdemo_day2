b2a553ce3f872aade38462cc2f400ad02e50d83f
rizul <rizulgoyal23@gmail.com>
2020-01-10 16:27:48 -0500

    all code

--- mapdemo_day2/ViewController.swift
+++ mapdemo_day2/ViewController.swift
@@ -7,14 +7,81 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
 
+    let places = Place.getPlaces()
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.showsUserLocation = true
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        addAnnotations()
+        addPolyLine()
+    //    locationManager.startUpdatingLocation()
+        
         // Do any additional setup after loading the view.
+        
+    }
+    
+    func addAnnotations()
+    {
+        mapView.delegate = self
+        mapView.addAnnotations(places)
+        
+        let overlays = places.map{(MKCircle(center: $0.coordinate, radius: 1000))}
+        mapView.addOverlays(overlays)
+    }
+    
+    func addPolyLine()
+    {
+        let locations = places.map{$0.coordinate}
+        let polyline = MKPolyline(coordinates: locations, count: locations.count)
+        mapView.addOverlay(polyline)
     }
 
 
+}
+
+extension ViewController: MKMapViewDelegate
+{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation
+        {
+            return nil
+        }
+        else
+        {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "ic_place_2x")
+            return annotationView
+        }
+    }
+    
+   // this func is needed  to add the overlays
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+        let renderer = MKCircleRenderer(overlay: overlay)
+        renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 2
+        return renderer
+    }
+        else if overlay is MKPolyline{
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.blue
+            renderer.lineWidth = 3
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
 }
 
