80a4ff48460acaf4d3a87c623b1fe117e81c6f74
rizul <rizulgoyal23@gmail.com>
2020-01-10 17:04:05 -0500

    all code done

--- mapdemo_day2/ViewController.swift
+++ mapdemo_day2/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         addAnnotations()
         addPolyLine()
+        addPolygon()
     //    locationManager.startUpdatingLocation()
         
         // Do any additional setup after loading the view.
@@ -47,6 +48,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let polyline = MKPolyline(coordinates: locations, count: locations.count)
         mapView.addOverlay(polyline)
     }
+    func addPolygon()
+    {
+        let locations = places.map{$0.coordinate}
+        let polygon = MKPolygon(coordinates: locations, count: locations.count)
+        mapView.addOverlay(polygon)
+    }
 
 
 }
@@ -62,6 +69,8 @@ extension ViewController: MKMapViewDelegate
         {
             let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
             annotationView.image = UIImage(named: "ic_place_2x")
+            annotationView.canShowCallout = true
+            annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
             return annotationView
         }
     }
@@ -81,7 +90,24 @@ extension ViewController: MKMapViewDelegate
             renderer.lineWidth = 3
             return renderer
         }
+        else if overlay is MKPolygon{
+            let renderer = MKPolygonRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.orange.withAlphaComponent(0.4)
+            renderer.strokeColor = UIColor.red
+            renderer.lineWidth = 3
+            return renderer
+        }
         return MKOverlayRenderer()
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let annotation = view.annotation as? Place, let title = annotation.title else {
+            return
+        }
+        let alertControl = UIAlertController(title: "Welcome to \(title)", message: "Have a glood time in \(title)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertControl.addAction(cancelAction)
+        present(alertControl, animated: true, completion: nil)
     }
 }
 
