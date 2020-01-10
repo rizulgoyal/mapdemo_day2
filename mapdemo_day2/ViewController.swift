87e63f13e4aeeca991f8200d95ac8709568507c7
rizul <rizulgoyal23@gmail.com>
2020-01-10 15:15:39 -0500

    Initial Commit

---
+++ mapdemo_day2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapdemo_day2
+//
+//  Created by Rizul goyal on 2020-01-10.
+//  Copyright © 2020 Rizul goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
